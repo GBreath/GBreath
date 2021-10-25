ddfede419a47d47a655170d399c84655ca18c1f1
PatoGordo <59514690+PatoGordo@users.noreply.github.com>
2021-10-25 00:43:57 -0300

    Update vibrate.ts

--- src/data/vibrate.ts
+++ src/data/vibrate.ts
@@ -1,6 +1,6 @@
 const inhaleVibrate = [200, 100, 200];
 const exhaleVibrate = [200, 100, 200, 100, 200];
-const holdVibrate = [700];
+const holdVibrate = [500];
 const finishVibrate = [1200];
 
 export { inhaleVibrate, exhaleVibrate, holdVibrate, finishVibrate };
